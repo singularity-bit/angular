a33cdd96ff01d4265d08fe043c1f4bfc523eb789
Vasile Grosu <vasilegrosu@Vasiles-MacBook-Pro.local>
2022-09-02 12:58:06 +0300

    recreated base app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  imports:[
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations:[
+    AppComponent
+  ],
+  bootstrap:[AppComponent]
 })
-export class AppModule { }
+export class AppModule{
+
+}
