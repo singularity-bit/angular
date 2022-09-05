de19c4958fc4161b1cb577ec6ca7ed71b3e5b68c
Vasile Grosu <vasilegrosu@Vasiles-MacBook-Pro.local>
2022-09-05 14:01:38 +0300

    played with pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,35 @@
 import { Component } from "@angular/core";
 @Component({
-  selector:'app-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls:['./app.component.css'],
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent{
-  name='q-singularity'
-  age='24'
-  logo='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
+export class AppComponent {
+  name = 'q-singularity'
+  age = '24'
+  logo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
+  currentDate = new Date()
+  cost = 2000
+  temperature = 25.3
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large'
+  }
 
-  getName(){
+  getName() {
     return this.name
   }
-  getAge(){
+  getAge() {
     return this.age
   }
-  setLogo(url:string){
-    this.logo=url;
+  setLogo(url: string) {
+    this.logo = url;
   }
-  onChangeLogo(event:KeyboardEvent){
-    this.logo=(event.target as HTMLInputElement).value
+  onChangeLogo(event: KeyboardEvent) {
+    this.logo = (event.target as HTMLInputElement).value
   }
-  logImg(event:string){
-    console.log('clicked child',event)
+  logImg(event: string) {
+    console.log('clicked child', event)
   }
 
 }
